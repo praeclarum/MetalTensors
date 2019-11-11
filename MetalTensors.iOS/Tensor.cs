979199de147a008bc0e0bb9278dd144aac8b89c8
Frank A. Krueger <fak@praeclarum.org>
2019-11-11 13:48:25 -0800

    Add ZeroTensor

--- MetalTensors.iOS/Tensor.cs
+++ MetalTensors.iOS/Tensor.cs
@@ -8,5 +8,13 @@ namespace MetalTensors
     public abstract class Tensor
     {
         public abstract int[] Shape { get; }
+
+        public void ValidateShape (int[] shape, string argumentName = "shape")
+        {
+            for (var i = 0; i < shape.Length; i++) {
+                if (shape[i] <= 0)
+                    throw new ArgumentOutOfRangeException (argumentName, $"Shape dimension must be > 0");
+            }
+        }
     }
 }
---
+++ MetalTensors.iOS/Tensors/ZeroTensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MetalTensors.Tensors
+{
+    public class ZeroTensor : Tensor
+    {
+        int[] shape;
+
+        public override int[] Shape => shape;
+
+        public ZeroTensor (int[] shape)
+        {
+            ValidateShape (shape);
+            this.shape = shape;
+        }
+    }
+}
