0dd0be203b120cb9eb6d7a1356f6bff0755d01f0
Frank A. Krueger <fak@praeclarum.org>
2019-11-11 13:37:57 -0800

    Initial commit

---
+++ MetalTensors.iOS/Tensor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetalTensors
+{
+    public abstract class Tensor
+    {
+        public abstract int[] Shape { get; }
+    }
+}
