4240a2057ddede9d207d5171bdb9ba0e1dc12d5a
Frank A. Krueger <fak@praeclarum.org>
2019-11-14 20:22:27 -0800

    Move shape extensions to tensor extensions

--- MetalTensors.iOS/MetalExtensions.cs
+++ MetalTensors.iOS/MetalExtensions.cs
@@ -43,49 +43,6 @@ namespace MetalTensors
             }
         }
 
-        public static int GetShapeLength (this int[] shape)
-        {
-            var r = 1;
-            for (var i = 0; i < shape.Length; i++) {
-                r *= shape[i];
-            }
-            return r;
-        }
-
-        static readonly int[] oneShape = { 1 };
-
-        public static int[] NormalizeShape (this int[]? shape)
-        {
-            if (shape is null)
-                return oneShape;
-
-            var n = shape.Length;
-            if (n <= 0)
-                return oneShape;
-
-            for (var bi = 0; bi < n; bi++) {
-                if (shape[bi] <= 0) {
-                    var ns = new int[n];
-                    for (var i = 0; i < n; i++) {
-                        ns[i] = Math.Max (1, shape[i]);
-                    }
-                    return ns;
-                }
-            }
-            return shape;
-        }
-
-        public static bool ShapeEquals (this int[] shape, int[] other)
-        {
-            if (shape.Length != other.Length)
-                return false;
-            for (var i = 0; i < shape.Length; i++) {
-                if (shape[i] != other[i])
-                    return false;
-            }
-            return true;
-        }
-
         public static unsafe void Fill (this MPSImage image, float constant)
         {
             var dtype = image.PixelFormat;
--- MetalTensors.iOS/TensorExtensions.cs
+++ MetalTensors.iOS/TensorExtensions.cs
@@ -5,6 +5,49 @@ namespace MetalTensors
 {
     public static class TensorExtensions
     {
+        public static int GetShapeLength (this int[] shape)
+        {
+            var r = 1;
+            for (var i = 0; i < shape.Length; i++) {
+                r *= shape[i];
+            }
+            return r;
+        }
+
+        static readonly int[] oneShape = { 1 };
+
+        public static int[] NormalizeShape (this int[]? shape)
+        {
+            if (shape is null)
+                return oneShape;
+
+            var n = shape.Length;
+            if (n <= 0)
+                return oneShape;
+
+            for (var bi = 0; bi < n; bi++) {
+                if (shape[bi] <= 0) {
+                    var ns = new int[n];
+                    for (var i = 0; i < n; i++) {
+                        ns[i] = Math.Max (1, shape[i]);
+                    }
+                    return ns;
+                }
+            }
+            return shape;
+        }
+
+        public static bool ShapeEquals (this int[] shape, int[] other)
+        {
+            if (shape.Length != other.Length)
+                return false;
+            for (var i = 0; i < shape.Length; i++) {
+                if (shape[i] != other[i])
+                    return false;
+            }
+            return true;
+        }
+
         public static Tensor[] Map (this Tensor[] tensors, Dictionary<Tensor, Tensor> map)
         {
             var n = tensors.Length;
