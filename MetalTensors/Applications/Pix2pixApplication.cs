465501e29afb0aa652c05284eff1f60359b40dd6
Frank A. Krueger <fak@praeclarum.org>
2022-02-22 16:11:27 -0800

    Use Upsample in Pix2pix

--- MetalTensors/Applications/Pix2pixApplication.cs
+++ MetalTensors/Applications/Pix2pixApplication.cs
@@ -81,16 +81,16 @@ namespace MetalTensors.Applications
                     var downconv = input.Conv (innerNC, size: 4, stride: 2, bias: useBias);
                     var down = downconv;
                     var downsub = submodule != null ? down.Apply (submodule) : down;
-                    var up = downsub.ReLU ().ConvTranspose (outerNC, size: 4, stride: 2, bias: true).Tanh ();
-                    //var up = downsub.ReLU ().Upsample ().Conv (outerNC, size: 4, stride: 1, bias: true).Tanh ();
+                    //var up = downsub.ReLU ().ConvTranspose (outerNC, size: 4, stride: 2, bias: true).Tanh ();
+                    var up = downsub.ReLU ().Upsample ().Conv (outerNC, size: 4, stride: 1, bias: true).Tanh ();
                     return up.Model (input, "Generator");
                 }
                 else if (innermost) {
                     var downrelu = input.LeakyReLU (a: 0.2f);
                     var downconv = downrelu.Conv (innerNC, size: 4, stride: 2, bias: useBias);
                     var down = downconv;
-                    var up = down.ReLU ().ConvTranspose (outerNC, size: 4, stride: 2, bias: useBias).BatchNorm ();
-                    //var up = down.ReLU ().Upsample ().Conv (outerNC, size: 4, stride: 1, bias: useBias).BatchNorm ();
+                    //var up = down.ReLU ().ConvTranspose (outerNC, size: 4, stride: 2, bias: useBias).BatchNorm ();
+                    var up = down.ReLU ().Upsample ().Conv (outerNC, size: 4, stride: 1, bias: useBias).BatchNorm ();
                     return input.Concat (up).Model (input, name);
                 }
                 else {
@@ -99,8 +99,8 @@ namespace MetalTensors.Applications
                     var downnorm = downconv;//.BatchNorm ();
                     var down = downnorm;
                     var downsub = submodule != null ? down.Apply (submodule) : down;
-                    var up = downsub.ReLU ().ConvTranspose (outerNC, size: 4, stride: 2, bias: useBias).BatchNorm ();
-                    //var up = downsub.ReLU ().Upsample ().Conv (outerNC, size: 4, stride: 1, bias: useBias).BatchNorm ();
+                    //var up = downsub.ReLU ().ConvTranspose (outerNC, size: 4, stride: 2, bias: useBias).BatchNorm ();
+                    var up = downsub.ReLU ().Upsample ().Conv (outerNC, size: 4, stride: 1, bias: useBias).BatchNorm ();
                     var updrop = useDropout ? up.Dropout (0.5f) : up;
                     return input.Concat (updrop).Model (input, name);
                 }
